48d71675294951d012cef3b4ce24808e12aeebec
Meir <nicolevianth@gmail.com>
2022-01-28 15:09:54 +0700

    Updated element classes

--- HiddenBlocking.normal.js
+++ HiddenBlocking.normal.js
@@ -8,9 +8,9 @@ for (var i = 0; i < blocked_users.length; i++) block[blocked_users[i]] = true;
 
 // Begin blocking
 function hiddenBlock() {
-	var messages = document.getElementsByClassName("messageListItem-1-jvGY"), startBlocking = false;
+	var messages = document.getElementsByClassName("messageListItem-ZZ7v6g"), startBlocking = false;
 	for (var i = 0; i < messages.length; i++) {
-		var message = messages[i], avatar = message.getElementsByClassName("avatar-1BDn8e");
+		var message = messages[i], avatar = message.getElementsByClassName("avatar-2e8lTP");
 		if (avatar.length) {
 			var src = !avatar[0].src.includes("guilds") ? avatar[0].src.substr(avatar[0].src.indexOf("/avatars/") + 9, 18) : avatar[0].src.substr(avatar[0].src.indexOf("/users/") + 7, 18);
 			if (block[src]) {
